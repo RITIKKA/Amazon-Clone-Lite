ab6d8a4adfcd3df41fedb24c59075a2818d348d4
RITIKKA <96379700+RITIKKA@users.noreply.github.com>
2022-08-15 19:27:34 +0530

    updated

--- amazon-clone/src/Components/Home/Home.js
+++ amazon-clone/src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { MdRepeat } from "react-icons/md";
 import background_img from "../Home/e-commerce.jpg";
 
 const Home = () => {
     return (
         <div>
-            <img style={{ backgroundImage: `url(${background_img})`, height:"500px", width:"100%" }} />
+            <img style={{ backgroundImage: `url(${background_img})`, backgroundSize:'cover', backgroundPosition:'center center', backgroundRepeat:'no-repeat', height:'500px', width:'100%' }} />
+            {/* <img src={background_img} style={{height:'500px', width:'100%'}}></img> */}
         </div>
     )
 }
